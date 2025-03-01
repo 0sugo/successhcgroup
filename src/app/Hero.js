40e03b8733754111d4fe8684583980564a6180fe
0sugo <josugo26@gmail.com>
2025-03-01 11:49:27 +0300

    Add Hero section

--- src/app/Hero.js
+++ src/app/Hero.js
@@ -12,9 +12,9 @@ const poppins = localFont({
 
 const Hero = () => {
   return (
-    <div className="relative">
-      <Image src={hero} alt="hero" className="rounded-lg" />
-      <div className=" bg-[#ed1352] flex flex-col gap-5 opacity-80 absolute top-1/2 transform  -translate-y-2/3 rounded-r-2xl text-white p-4">
+    <div className="relative rounded-2xl">
+      <Image src={hero} alt="hero" className="rounded-2xl" />
+      <div className=" bg-[#ed1352] flex flex-col gap-5 opacity-80 absolute top-1/2 transform -translate-y-2/3 rounded-r-2xl text-white p-4">
         <h1 className={`text-7xl text-[#E2E2D2] font-semibold text-center ${rhoza.className}`}>
           NDIS <br /> Disability <br /> Support
         </h1>
