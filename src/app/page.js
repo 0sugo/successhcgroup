229e1744e58068019d8ce7605b1a8082e69370b6
0sugo <josugo26@gmail.com>
2025-03-01 11:49:47 +0300

    Add Page section

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,27 @@
 import Image from "next/image";
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import About from "./About";
+import Services from "./Services";
+import Topbar from "./Topbar";
+import Values from "./Values";
+import Footer from "./Footer";
+
 
 export default function Home() {
   return (
-    <div className="bg-white">
-      <div className="w-[90%] mx-auto ">
+    <div className={` bg-white`}>
+      <Topbar />
+      <div className="w-[90%] mx-auto space-y-14  ">
         <div className="h-screen overflow-y-hidden">
           <Navbar />
           <Hero />
         </div>
+        <Services />
+        <About  />
+        <Values />
       </div>
+      <Footer />
     </div>
   );
 }
