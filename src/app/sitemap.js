f344a02f4499d3aff626cd00cecd1ac9d5376ba9
0sugo <josugo26@gmail.com>
2025-03-18 10:36:24 +0300

    Add sitemap file

---
+++ src/app/sitemap.js
@@ -0,0 +1,40 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://successhcgroup.com.au/',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+      {
+        url: 'https://successhcgroup.com.au/about',
+        lastModified: new Date(),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://successhcgroup.com.au/services',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://successhcgroup.com.au/contact',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://successhcgroup.com.au/jobs',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://successhcgroup.com.au/services',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+    ]
+  }
