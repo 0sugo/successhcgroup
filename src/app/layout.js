89f126db7d68f386b43ca0a50193fc4d9664aea4
0sugo <josugo26@gmail.com>
2025-03-25 19:12:36 +0300

    Update layout file

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,7 @@ const poppins = localFont({
 
 export const metadata = {
   metadataBase: new URL("https://successhcgroup.com.au/"),
+  icons: "/clearLogo.jpg",
   keywords: "Success Healthcare Group,Success,Successhcgroup,success group,Success healthcare, NDIS, Disability, Support, Services,staffing,staffing agency,NDIS provider,NDIS services,NDIS support,NDIS support coordination,NDIS support services,NDIS support worker,NDIS support workers,NDIS supports,NDIS therapy,NDIS training,NDIS transport,NDIS worker,NDIS workers,NDIS workshop,NDIS workshops,NDIS youth,NDIS youth services,NDIS youth support,NDIS youth support services,NDIS youth worker,NDIS youth workers,NDIS youth workshops,NDIS youth workshops",
   title:{
     default: "SuccessHealthcareGroup",
