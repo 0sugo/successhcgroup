d47f186304db832f98cf251a5cfdb575e8908088
0sugo <josugo26@gmail.com>
2025-03-25 22:59:34 +0300

    Update layout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
+import Topbar from "./Topbar";
+import Navbar from "./Navbar";
+import ChatBubble from "./chatBubble";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,24 +18,22 @@ const poppins = localFont({
   src: "/fonts/Poppins-Regular.ttf",
 });
 
-
-
 export const metadata = {
   metadataBase: new URL("https://successhcgroup.com.au/"),
   icons: "/clearLogo.jpg",
-  keywords: "Success Healthcare Group,Success,Successhcgroup,success group,Success healthcare, NDIS, Disability, Support, Services,staffing,staffing agency,NDIS provider,NDIS services,NDIS support,NDIS support coordination,NDIS support services,NDIS support worker,NDIS support workers,NDIS supports,NDIS therapy,NDIS training,NDIS transport,NDIS worker,NDIS workers,NDIS workshop,NDIS workshops,NDIS youth,NDIS youth services,NDIS youth support,NDIS youth support services,NDIS youth worker,NDIS youth workers,NDIS youth workshops,NDIS youth workshops",
-  title:{
+  keywords:
+    "Success Healthcare Group,Success,Successhcgroup,success group,Success healthcare, NDIS, Disability, Support, Services,staffing,staffing agency,NDIS provider,NDIS services,NDIS support,NDIS support coordination,NDIS support services,NDIS support worker,NDIS support workers,NDIS supports,NDIS therapy,NDIS training,NDIS transport,NDIS worker,NDIS workers,NDIS workshop,NDIS workshops,NDIS youth,NDIS youth services,NDIS youth support,NDIS youth support services,NDIS youth worker,NDIS youth workers,NDIS youth workshops,NDIS youth workshops",
+  title: {
     default: "Success Healthcare Group",
-    template:`$s | SuccessHealthcareGroup`,
+    template: `$s | SuccessHealthcareGroup`,
   },
   openGraph: {
     url: "https://successhcgroup.com.au/",
     site_name: "Success Healthcare Group",
-    description: 'Your Success, Our Promise',
-    images: ['']
+    description: "Your Success, Our Promise",
+    images: [""],
   },
-}
- 
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -40,7 +41,12 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${poppins.className} ${geistMono.variable} antialiased`}
       >
+        <Topbar />
+        <div className="w-[90%] mx-auto">
+          <Navbar />
+        </div>
         {children}
+        <ChatBubble />
       </body>
     </html>
   );
