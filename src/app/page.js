862aa6c2838adab38cad68542a1df838f29403b9
0sugo <josugo26@gmail.com>
2025-07-30 22:36:36 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,20 @@
-import Image from "next/image";
-import Navbar from "./Navbar";
 import Hero from "./Hero";
 import About from "./About";
 import Services from "./Services";
-import Topbar from "./Topbar";
 import Values from "./Values";
 import Footer from "./Footer";
-
+import Sales from "./Sales";
 
 export default function Home() {
   return (
     <div className={` bg-white overflow-x-hidden`}>
-      {/* <Topbar /> */}
+        <Hero />
       <div className="w-[90%] mx-auto ">
-          <Hero />
         <Services />
-        <About  />
+        </div>
+        <Sales />
+      <div className="w-[90%] mx-auto ">
+        <About />
         <Values />
       </div>
       <Footer />
