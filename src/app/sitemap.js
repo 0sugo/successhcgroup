315ce6ecb8ff30a282279565a52af3ac64923049
0sugo <josugo26@gmail.com>
2025-09-28 16:13:40 +0300

    Update sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,40 +1,38 @@
+export const dynamic = "force-static";
+
 export default function sitemap() {
-    return [
-      {
-        url: 'https://successhcgroup.com.au/',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 1,
-      },
-      {
-        url: 'https://successhcgroup.com.au/about',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://successhcgroup.com.au/services',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-      {
-        url: 'https://successhcgroup.com.au/contact',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-      {
-        url: 'https://successhcgroup.com.au/jobs',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-      {
-        url: 'https://successhcgroup.com.au/services',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.5,
-      },
-    ]
-  }
+  const now = new Date().toISOString();
+
+  return [
+    {
+      url: 'https://successhcgroup.com.au/',
+      lastModified: now,
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://successhcgroup.com.au/about',
+      lastModified: now,
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://successhcgroup.com.au/services',
+      lastModified: now,
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://successhcgroup.com.au/contact',
+      lastModified: now,
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://successhcgroup.com.au/jobs',
+      lastModified: now,
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    }
+  ];
+}
