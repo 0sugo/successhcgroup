a8af5c7510c2bdc68ca7e4172c1a858bba3d016b
0sugo <josugo26@gmail.com>
2025-12-18 21:33:23 +0800

    Revert "Add assets"
    This reverts commit 3a3abc87aebdb48706c89b6dbd3cd84fb87fbf8e.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,54 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Inter, JetBrains_Mono } from 'next/font/google';
+import localFont from "next/font/local";
+import Topbar from "./Topbar";
+import Navbar from "./Navbar";
+import ChatBubble from "./chatBubble";
 
-const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-  display: 'swap',
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ['latin'],
-  variable: '--font-jetbrains-mono',
-  display: 'swap',
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+const poppins = localFont({
+  src: "/fonts/Poppins-Regular.ttf",
 });
 
 export const metadata = {
-  title: "Success Healthcare Group",
-  description: "Healthcare Staffing and Services",
+  metadataBase: new URL("https://successhcgroup.com.au/"),
+  icons: "/clearLogo.jpg",
+  keywords:
+    "Success Healthcare Group,success home care services, success home care,Success,Successhcgroup,success group,Success healthcare, NDIS, Disability, Support, Services,staffing,staffing agency,NDIS provider,NDIS services,NDIS support,NDIS support coordination,NDIS support services,NDIS support worker,NDIS support workers,NDIS supports,NDIS therapy,NDIS training,NDIS transport,NDIS worker,NDIS workers,NDIS workshop,NDIS workshops,NDIS youth,NDIS youth services,NDIS youth support,NDIS youth support services,NDIS youth worker,NDIS youth workers,NDIS youth workshops,NDIS youth workshops",
+  title: {
+    default: "Success Healthcare Group",
+    template: `$s | SuccessHealthcareGroup`,
+  },
+  openGraph: {
+    url: "https://successhcgroup.com.au/",
+    site_name: "Success Healthcare Group",
+    description: "Your Success, Our Promise",
+    images: [""],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.variable} ${jetbrainsMono.variable}`}>
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${poppins.className} ${geistMono.variable} antialiased`}
+      >
+        <Topbar />
+        <div className="w-[90%] mx-auto">
+          <Navbar />
+        </div>
+        {children}
+        {/* <ChatBubble /> */}
+        <elevenlabs-convai agent-id="aRLmOTy9tUkTkVvELKnr"></elevenlabs-convai><script src="https://elevenlabs.io/convai-widget/index.js" async type="text/javascript"></script>
+      </body>
     </html>
   );
 }
